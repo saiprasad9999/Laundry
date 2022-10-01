b15505dc60b9c363aea30d33553b415295a4c782
Sai <saiprasadra99@gmail.com>
2022-10-01 15:17:01 +0530

    first

---
+++ EasyWash/src/main/java/com/easywash/EasyWashApplication.java
@@ -0,0 +1,13 @@
+package com.easywash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyWashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyWashApplication.class, args);
+	}
+
+}
---
+++ EasyWash/src/test/java/com/easywash/EasyWashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easywash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyWashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
